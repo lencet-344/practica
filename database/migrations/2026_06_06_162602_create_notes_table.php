2b11cdeef614c5ed82493cb761ee9bbaaabd2e14
lencet-344 <odoricozeleherrera@gmail.com>
2026-06-06 10:35:52 -0600

    feat: add foreign key constraint for category_id in notes table.

--- database/migrations/2026_06_06_162602_create_notes_table.php
+++ database/migrations/2026_06_06_162602_create_notes_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->increments("id");
             $table->string("title");
 
-            
+            $table->integer("category_id")->unsigned();
+            $table->foreign("category_id")->references("id")->on("categories")->onDelete("cascade");
 
             $table->timestamps();
         });
